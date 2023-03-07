d73b5b934518e4a47bd77ea74dcf958f5644f44e
RukayaOK <34509633+RukayaOK@users.noreply.github.com>
2023-03-07 20:15:59 +0000

    initial commit

---
+++ loop_array_through_function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function loop_array_through_function() {
+   arr=("$@")
+   for i in "${arr[@]}";
+      do
+          echo "$i"
+      done
+
+}
+
+array=("one 1" "two 2" "three 3")
+
+copyFiles "${array[@]}"
