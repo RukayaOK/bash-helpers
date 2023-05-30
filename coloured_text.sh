05fb993cb17c7028dde4394a15a8540e85654616
Rukaya Ogazi-Khan <34509633+RukayaOK@users.noreply.github.com>
2023-05-30 12:45:40 +0100

    Create coloured_text.sh

---
+++ coloured_text.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+wipe="\033[1m\033[0m"
+
+_information() {
+    _color='\033[0;35m' #cyan
+    echo "${_color} $1 ${wipe}"
+}
+
+_success() {
+    _color='\033[0;32m' #green
+    echo "${_color} $1 ${wipe}"
+}
